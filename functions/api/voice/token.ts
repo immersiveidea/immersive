9a23df34fb3b158706b156c6a3bb1bbb18ccbeaf
Michael Mainguy <mike.mainguy@gmail.com>
2023-08-10 13:21:38 -0500

    Changed from axios to fetch.

--- functions/api/voice/token.ts
+++ functions/api/voice/token.ts
@@ -1,13 +1,18 @@
 interface Env {
     VOICE_TOKEN: string;
 }
-export const onRequest: PagesFunction<Env> = async (context) => {
+
+addEventListener('fetch', event => {
+    event.respondWith(eventHandler(event));
+});
+
+async function eventHandler(event) {
     try {
         const res = await fetch('https://api.assemblyai.com/v2/realtime/token',
             {
                 method: 'POST',
                 body: JSON.stringify({expires_in: 3600}),
-                headers: {authorization: context.env.VOICE_TOKEN}
+                headers: {authorization: event.context.env.VOICE_TOKEN}
             });
         const response = await res.json();
         return new Response(JSON.stringify(response), {status: 200});
