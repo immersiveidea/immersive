19022194501814df3e81f8bbe276aa84cf49730f
Michael Mainguy <mike.mainguy@gmail.com>
2023-08-10 14:15:19 -0500

    Changed from axios to fetch.

--- functions/api/voice/token.ts
+++ functions/api/voice/token.ts
@@ -2,21 +2,36 @@ interface Env {
     VOICE_TOKEN: string;
 }
 
-addEventListener('fetch', event => {
-    event.respondWith(eventHandler(event));
-});
+const handler: ExportedHandler<Env> = {
+    async fetch(request, env: Env, context) {
+        async function gatherResponse(response) {
+            const {headers} = response;
+            const contentType = headers.get("content-type") || "";
+            if (contentType.includes("application/json")) {
+                return JSON.stringify(await response.json());
+            }
+            return response.text();
+        }
 
-async function eventHandler(event) {
+        const init = {
+            method: 'POST',
+            body: JSON.stringify({expires_in: 3600}),
+            headers: {authorization: env.VOICE_TOKEN}
+        };
+        const response = await fetch('https://api.assemblyai.com/v2/realtime/token', init);
+        const results = await gatherResponse(response);
+        return new Response(results, init);
+    }
+}
+export default handler;
+/*
+export const onRequest: PagesFunction<Env> = async (context) => {
     try {
         const res = await fetch('https://api.assemblyai.com/v2/realtime/token',
-            {
-                method: 'POST',
-                body: JSON.stringify({expires_in: 3600}),
-                headers: {authorization: event.context.env.VOICE_TOKEN}
-            });
+            {method: 'POST', body: JSON.stringify({expires_in: 3600}), headers: {authorization: context.env.VOICE_TOKEN}});
         const response = await res.json();
-        return new Response(JSON.stringify(response), {status: 200});
+        return Response.json(response);
     } catch (error) {
         return new Response(error.message, {status: 500});
     }
-}
+}*/
