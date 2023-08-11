f27e3dd3cdfcca7996553a74a695a538acd9353f
Michael Mainguy <mike.mainguy@gmail.com>
2023-08-11 06:42:58 -0500

    changed to simple response.

--- functions/api/voice/token.ts
+++ functions/api/voice/token.ts
@@ -1,34 +1,11 @@
 interface Env {
     VOICE_TOKEN: string;
 }
-export const onRequest: PagesFunction<Env> = async (context) => {
-    try {
-        console.log('start');
 
-        async function gatherResponse(response: Response) {
-            const {headers} = response;
-            const contentType = headers.get('content-type') || '';
-            if (contentType.includes('application/json')) {
-                return await response.text();
-            } else if (contentType.includes('application/text')) {
-                return await response.text();
-            } else if (contentType.includes('text/html')) {
-                return await response.text();
-            } else {
-                return await response.text();
-            }
-        }
-
-        const res = await fetch('https://api.assemblyai.com/v2/realtime/token',
-            {
-                method: 'POST',
-                body: JSON.stringify({expires_in: 3600}),
-                headers: {authorization: context.env.VOICE_TOKEN}
-            });
-        const results = await gatherResponse(res);
-
-        return new Response(results, {status: 200});
-    } catch (error) {
-        return new Response(error.message, {status: 500});
+const handler: ExportedHandler<Env> = {
+    async fetch(request, context) {
+        console.log(context.VOICE_TOKEN);
+        return new Response('Hello World 2!');
     }
 }
+export default handler;
