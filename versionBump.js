ae73f3e74b531bf66088f76670365517b4538a58
Michael Mainguy <mike.mainguy@gmail.com>
2024-06-11 16:51:27 -0500

    Added smart versioning

--- versionBump.js
+++ versionBump.js
@@ -19,7 +19,6 @@ const getPackageJson = async () => {
     json.version = newVersion;
     await fs.writeFile('package.json', JSON.stringify(json, null, 2));
     let indexHtml = await fs.readFile('index.html', 'utf8');
-    console.log(indexHtml);
     indexHtml = indexHtml.replace('@@VERSION', newVersion);
     indexHtml = indexHtml.replace('@@DATE', new Date().toISOString());
     const gitId = (await getGitId()).replace('\n', '');
